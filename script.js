d279066dfe3388a8f0eeb454031b6bce1c0fec57
martincandra <80158888+martincandra@users.noreply.github.com>
2022-06-29 13:45:12 +0700

    upload website pribadi 2

---
+++ script.js
@@ -0,0 +1,42 @@
+const txtElement = ['Martin Candra Wijaya']
+let count = 0;
+let txtIndex =0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count]
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 250);
+})();
+
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 700) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
